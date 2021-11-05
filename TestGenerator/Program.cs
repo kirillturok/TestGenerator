014d59f1aec3a93eb0f3b930dfb57dc7c0ab93f6
Kiryl <kirillturok1@gmail.com>
2021-11-05 12:40:55 +0300

    Добавьте файлы проекта.

---
+++ TestGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
